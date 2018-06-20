b03b5c29058e9112aa8e131ee688650c4ff55ec2
James Mills <james@jamesmills.co.uk>
2018-06-20 16:23:54 +0400

    Sorting out the admin navigation

--- publish/resources/views/admin/sidebar.blade.php
+++ publish/resources/views/admin/sidebar.blade.php
@@ -17,29 +17,27 @@
 
     <div class="card">
         <div class="card-header">
-            Users
+            Users / Roles / Permissions
         </div>
         <div class="card-body">
             <ul class="nav flex-column" role="tablist">
                 <li class="nav-item" role="presentation">
-                    <a href="{{ route('admin.users.index') }}">Show all</a>
-                    <a href="{{ route('admin.users.create') }}">Create</a>
+                    <a href="{{ route('admin.users.index') }}">All users</a>
                 </li>
-            </ul>
-        </div>
-    </div>
-
-    <br/>
-
-    <div class="card">
-        <div class="card-header">
-            Roles
-        </div>
-        <div class="card-body">
-            <ul class="nav flex-column" role="tablist">
                 <li class="nav-item" role="presentation">
-                    <a href="{{ route('admin.roles.index') }}">Show all</a>
-                    <a href="{{ route('admin.roles.create') }}">Create</a>
+                    <a href="{{ route('admin.users.create') }}">Create user</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a href="{{ route('admin.roles.index') }}">All roles</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a href="{{ route('admin.roles.create') }}">Create role</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a href="{{ route('admin.permissions.index') }}">All permissions</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a href="{{ route('admin.permissions.create') }}">Create permission</a>
                 </li>
             </ul>
         </div>
