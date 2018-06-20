4abf597be3df91f892e99e284d6e08ba06ae683c
James Mills <james@jamesmills.co.uk>
2018-06-20 11:19:50 +0400

    Updating the format of the migration file

--- src/database/migrations/2016_01_01_193651_create_roles_permissions_tables.php
+++ src/database/migrations/2016_01_01_193651_create_roles_permissions_tables.php
@@ -30,15 +30,8 @@ class CreateRolesPermissionsTables extends Migration
             $table->integer('permission_id')->unsigned();
             $table->integer('role_id')->unsigned();
 
-            $table->foreign('permission_id')
-                ->references('id')
-                ->on('permissions')
-                ->onDelete('cascade');
-
-            $table->foreign('role_id')
-                ->references('id')
-                ->on('roles')
-                ->onDelete('cascade');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
 
             $table->primary(['permission_id', 'role_id']);
         });
@@ -47,15 +40,8 @@ class CreateRolesPermissionsTables extends Migration
             $table->integer('role_id')->unsigned();
             $table->integer('user_id')->unsigned();
 
-            $table->foreign('role_id')
-                ->references('id')
-                ->on('roles')
-                ->onDelete('cascade');
-
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users')
-                ->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->primary(['role_id', 'user_id']);
         });
